3c7e8d09df59f4782e8c7c7b4ab30fc0c38fbbf3
jjtovarl <jtovarl13@hotmail.com>
2014-09-19 08:09:56 -0500

    Update steps.py

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -10,26 +10,25 @@ def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, g
 
 @step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
 def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    assert False, 'This step must be implemented'
+    assert world.match.anotar(p1, int(group2[0]), group3, group4)
 
 
 @step(u'Then: I see score: "([^"]*)"')
 def then_i_see_score_group1(step, group1):
-    assert False, 'This step must be implemented'
+    assert world.match.score() == group1, 'Error got: '+ world.match.score()
 
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def when_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+	world.match.anotar(group1, int(group2[0]), group3, group4)
 
 
 @step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+    world.match.anotar(group1, int(group2[0]), group3, group4)
 
 
 @step(u'Then: The match score is: "([^"]*)"')
 def then_the_match_score_is_group1(step, group1):
-    assert False, 'This step must be implemented'
-
+    assert world.match.score() == group1, "Got: " % world.match.score()
 
